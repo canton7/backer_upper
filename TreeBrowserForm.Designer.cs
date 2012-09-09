8c56b99da5f4b5975c6a6dcbce792e668170737e
Antony Male <antony.male@gmail.com>
2012-09-09 02:26:50 +0100

    Tidy up FileBrowserForm

--- TreeBrowserForm.Designer.cs
+++ TreeBrowserForm.Designer.cs
@@ -25,6 +25,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TreeBrowserForm));
             this.buttonOK = new System.Windows.Forms.Button();
             this.textBoxSource = new System.Windows.Forms.TextBox();
             this.buttonBrowse = new System.Windows.Forms.Button();
@@ -34,12 +35,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxIgnorePattern = new System.Windows.Forms.TextBox();
             this.fileTreeBrowser = new BackerUpper.FileTreeBrowser();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonOK
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonOK.Location = new System.Drawing.Point(12, 423);
+            this.buttonOK.Location = new System.Drawing.Point(12, 442);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 1;
@@ -51,7 +53,7 @@
             // 
             this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxSource.Location = new System.Drawing.Point(13, 33);
+            this.textBoxSource.Location = new System.Drawing.Point(12, 43);
             this.textBoxSource.Name = "textBoxSource";
             this.textBoxSource.Size = new System.Drawing.Size(259, 20);
             this.textBoxSource.TabIndex = 2;
@@ -61,7 +63,7 @@
             // 
             this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonBrowse.Image = global::BackerUpper.Properties.Resources.IconFolderOpen;
-            this.buttonBrowse.Location = new System.Drawing.Point(278, 31);
+            this.buttonBrowse.Location = new System.Drawing.Point(277, 40);
             this.buttonBrowse.Name = "buttonBrowse";
             this.buttonBrowse.Size = new System.Drawing.Size(26, 23);
             this.buttonBrowse.TabIndex = 3;
@@ -76,7 +78,7 @@
             // 
             this.labelValidSource.AutoSize = true;
             this.labelValidSource.BackColor = System.Drawing.Color.White;
-            this.labelValidSource.Location = new System.Drawing.Point(17, 66);
+            this.labelValidSource.Location = new System.Drawing.Point(16, 75);
             this.labelValidSource.Name = "labelValidSource";
             this.labelValidSource.Size = new System.Drawing.Size(139, 13);
             this.labelValidSource.TabIndex = 4;
@@ -84,7 +86,8 @@
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(93, 423);
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCancel.Location = new System.Drawing.Point(93, 440);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 5;
@@ -96,7 +99,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 398);
+            this.label1.Location = new System.Drawing.Point(12, 417);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 6;
@@ -106,7 +109,7 @@
             // 
             this.textBoxIgnorePattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxIgnorePattern.Location = new System.Drawing.Point(47, 395);
+            this.textBoxIgnorePattern.Location = new System.Drawing.Point(47, 414);
             this.textBoxIgnorePattern.Name = "textBoxIgnorePattern";
             this.textBoxIgnorePattern.Size = new System.Drawing.Size(257, 20);
             this.textBoxIgnorePattern.TabIndex = 7;
@@ -117,16 +120,26 @@
             this.fileTreeBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.fileTreeBrowser.Location = new System.Drawing.Point(12, 59);
+            this.fileTreeBrowser.Location = new System.Drawing.Point(12, 69);
             this.fileTreeBrowser.Name = "fileTreeBrowser";
-            this.fileTreeBrowser.Size = new System.Drawing.Size(292, 330);
+            this.fileTreeBrowser.Size = new System.Drawing.Size(292, 339);
             this.fileTreeBrowser.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Select exactly which files and folders get backed up";
             // 
             // TreeBrowserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 458);
+            this.ClientSize = new System.Drawing.Size(316, 477);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxIgnorePattern);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonCancel);
@@ -135,8 +148,9 @@
             this.Controls.Add(this.textBoxSource);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.fileTreeBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TreeBrowserForm";
-            this.Text = "TreeBrowserForm";
+            this.Text = "Advanced Source Settings";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,5 +167,6 @@
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxIgnorePattern;
+        private System.Windows.Forms.Label label2;
     }
 }
